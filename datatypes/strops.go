5d2ff184533e953774c60316550497a6db7173a8
Ashutoshh <asashuthegreat4@gmail.com>
2022-04-12 03:14:36 -0700

    Add files via upload

--- datatypes/strops.go
+++ datatypes/strops.go
@@ -2,11 +2,13 @@ package main
 import (
 	"fmt"
 	"time"
+	"strings"
 ) 
 
 func stops() {
 	var st1 string = "Hello"
 	var st2 string 
+	data := "hello oracle this is golang training"
 	fmt.Println("enter any string : ")
 	fmt.Scan(&st2)
 	time.Sleep(1*time.Second)
@@ -19,6 +21,15 @@ func stops() {
 	fmt.Println(st2[1]) // ascii value of char
 	fmt.Println(string(st2[1])) // value of index 
 	fmt.Println(string(st2[1:4])) // range of string char
+
+	// advanced operations in string
+	// searching 
+	fmt.Println(strings.Contains(data,st2))
+	// counting 
+	fmt.Println(strings.Count(data,"t"))
+	// spliting 
+	data_split := strings.Split(data," ")
+	fmt.Println(data_split)
 
 
 }
