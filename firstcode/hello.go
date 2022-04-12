1b97142e9a1217c9b98db5ed513355d2ad295db3
Ashutoshh <asashuthegreat4@gmail.com>
2022-04-11 22:47:35 -0700

    Add files via upload

---
+++ firstcode/hello.go
@@ -0,0 +1,16 @@
+// package is manadatory 
+package main
+// declaration of things we are using under code 
+import (
+	"fmt" 
+)
+
+// go compiler will start the code from main function only 
+func main() {
+	// print without new line 
+	fmt.Print("Hello world  !! ")
+	// print with new line 
+	fmt.Println("hello world with new line char..")
+	fmt.Println("hello guys ..")
+
+}
